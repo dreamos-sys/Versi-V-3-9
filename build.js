094bfecd7e6492f18b9f1c1d8dec6e8f877fc1ac
Dream <afumum234@gmail.com>
2026-01-19 13:03:24 +0700

    Update build.js

--- build.js
+++ build.js
@@ -1,11 +1,5 @@
 const fs = require('fs');
-const path = require('path');
-
-// Buat folder dist
-fs.mkdirSync('dist', { recursive: true });
-
-// Copy file
-fs.copyFileSync('index.html', 'dist/index.html');
-fs.copyFileSync('service-worker-nuclear.js', 'dist/service-worker-nuclear.js');
-
-console.log('✅ Build Success');
+  if (!fs.existsSync('dist')) fs.mkdirSync('dist');
+  fs.copyFileSync('index.html', 'dist/index.html');
+  fs.copyFileSync('service-worker-nuclear.js', 'dist/service-worker-nuclear.js');
+  console.log('✅ Build Success');
