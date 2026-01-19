cace1993aad5c24720acce8cd4384355acee4ba6
Dream <afumum234@gmail.com>
2026-01-19 10:38:32 +0700

    Delete build.mjs

--- build.mjs
+++
@@ -1,9 +0,0 @@
-import { cpSync, mkdirSync } from 'fs';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-mkdirSync('./dist', { recursive: true });
-cpSync('./index.html', './dist/index.html');
-cpSync('./service-worker-nuclear.js', './dist/service-worker-nuclear.js');
-console.log('✅ Build Success');
